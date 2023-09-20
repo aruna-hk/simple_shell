d05e97494a9c2bf615d1ae26d8845739695697f3
kiptoo haron <kiptooharon.hk@gmain.com>
2023-09-20 16:32:53 -0400

    corrected execute function

--- execute.c
+++ execute.c
@@ -10,10 +10,5 @@ int execute(char *str, char *name)
 	int i = 0;
 	char **ar_ =  arr(strdup(str));
 
-	while (ar_[i] != 0)
-	{
-		dprintf(STDOUT_FILENO, "%s\n", ar_[i]);
-		i++;
-	}
 	return (execute_line(ar_, name));
 }
